250272979ed42153aa554b98852a4c55a2969a84
inaarakheraj <57158055+inaarakheraj@users.noreply.github.com>
2019-12-09 11:48:59 -0500

    Update urls.py

--- squirrel_tracker/urls.py
+++ squirrel_tracker/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from tracking.views import map_view
+from tracking import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sightings/', include('tracking.urls')),
-    path('map/', map_view)
+    path('stats', views.stats),
+    path('add', views.add_sighting),
+    path('map/', map_view),
+    path('<str:pk>', views.update_or_delete)
 ]
