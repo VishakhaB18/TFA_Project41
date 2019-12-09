1e25646ff7581ceea30618c3a782cdce8aadad59
inaarakheraj <57158055+inaarakheraj@users.noreply.github.com>
2019-12-09 11:45:12 -0500

    Update settings.py

--- squirrel_tracker/settings.py
+++ squirrel_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a=yvvc^+t0fyah)08ls#hs4g@1v#z@+rga$y+66q%fdd+oyo=u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
